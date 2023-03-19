1269d8c09d8cb2a9bbd9b2a1e239a54d5469af24
ninja03 <69106571+ninja03@users.noreply.github.com>
2023-03-19 15:25:19 +0900

    Update db.ts

--- utils/db.ts
+++ utils/db.ts
@@ -1,7 +1,6 @@
 import * as postgres from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 
 const databaseUrl = Deno.env.get("DATABASE_URL");
-console.log("databaseUrl", databaseUrl);
 const pool = new postgres.Pool(databaseUrl, 3, true);
 
 export { pool };
